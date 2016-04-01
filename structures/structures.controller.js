c824b493db7d630c80fc15ebbfa216c8f9b9b109
Robert Herhold <reherhold@gmail.com>
2016-03-31 21:01:56 -0400

    Add endpoint to get a structure by ID

--- structures/structures.controller.js
+++ structures/structures.controller.js
@@ -5,11 +5,24 @@ var Structure = require('./Structure.model');
 
 exports.init = function(router, app) {
     router.post('/structures', createStructure);
+    router.get('/structures/:id', getStructure);
 }
 
-function *createStructure() {
+function* createStructure() {
     var structure = yield parse.json(this);
     structure = yield Structure.create(structure);
     this.status = 201;
-    this.body = structure;
+    this.body = {
+        callback: 'localhost:3000/structures/' + structure._id
+    };
+}
+
+function* getStructure() {
+    var structure = yield Structure.findOne({
+        '_id': this.params.id
+    }, '-__v').exec();
+    this.status = structure ? 200 : 404;
+    this.body = structure ? structure : {
+        message: 'Structure with ID ' + this.params.id + " not found."
+    }
 }
