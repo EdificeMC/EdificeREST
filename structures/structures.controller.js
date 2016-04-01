d17d1e641e373c90a21f07f9d52d72830c82a503
Robert Herhold <reherhold@gmail.com>
2016-03-31 20:32:01 -0400

    Move all data cleaning to the client

--- structures/structures.controller.js
+++ structures/structures.controller.js
@@ -9,34 +9,7 @@ exports.init = function(router, app) {
 
 function *createStructure() {
     var structure = yield parse.json(this);
-
-    for(let block of structure.blocks) {
-        sanitizeBlock(block, structure.originPosition);
-    }
-    delete structure.originPosition;
-
     structure = yield Structure.create(structure);
-
     this.status = 201;
     this.body = structure;
-}
-
-function sanitizeBlock(block, originPos) {
-    removeContentVersion(block)
-    delete block.WorldUuid;
-    block.Position.X -= originPos.X;
-    block.Position.Y -= originPos.Y;
-    block.Position.Z -= originPos.Z;
-}
-
-function removeContentVersion(block) {
-    for (let key in block) {
-        if (typeof block[key] == "object" && block[key] !== null) {
-            removeContentVersion(block[key]);
-        } else {
-            if(key === "ContentVersion") {
-                delete block[key];
-            }
-        }
-    }
 }
