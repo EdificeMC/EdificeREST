80500c426f80979e976e9c910efc76ec1f685c93
Robert Herhold <reherhold@gmail.com>
2016-05-23 23:15:56 -0400

    Update for aurelia-auth

--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -4,14 +4,20 @@ var parse = require('co-body');
 var http = require('axios');
 
 exports.init = function(router, app) {
-    router.post('/auth/', mojangAuth);
+    router.post('/auth', mojangAuth);
 }
 
 function* mojangAuth() {
     var body = yield parse.json(this);
+    body.username = body.email;
+    delete body.email;
+    body.agent = {
+        name: "Minecraft",
+        version: 1
+    }
 
     var mojangRes = yield http.post('https://authserver.mojang.com/authenticate', body);
-    
-    this.status = mojangRes.data.status;
-    this.body = mojangRes.data.data;
+
+    this.status = mojangRes.status;
+    this.body = mojangRes.data;
 }
