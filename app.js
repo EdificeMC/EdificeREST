8e65b5b870d73ab9f73391ece065488f76761f95
Robert Herhold <reherhold@gmail.com>
2016-06-18 01:09:51 -0400

    Remove koa static server

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ let app = koa({ name: 'Edifice' });
 
 app.use(require('koa-logger')());
 app.use(require('koa-cors')({ methods: ['GET', 'PUT', 'POST', 'DELETE'] }));
-app.use(require('koa-static-server')({ rootDir: 'EdificeWeb/dist' }));
 app.use(require('./middleware/boom'));
 app.use(bodyparser());
 
