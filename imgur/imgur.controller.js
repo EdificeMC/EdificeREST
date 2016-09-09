e50707a3befd5b032228e2c479398a7f6a12ea6f
Robert Herhold <RobertHerhold@users.noreply.github.com>
2016-09-08 21:54:10 -0400

    Image processing before Imgur upload (#5)
    * Autocrop and scale images

--- imgur/imgur.controller.js
+++ imgur/imgur.controller.js
@@ -1,19 +1,50 @@
 'use strict';
 
-var http = require('axios');
-var config = require('config');
+const http = require('axios');
+const config = require('config');
+const Jimp = require('jimp');
+
+const FULL_WIDTH = 500;
+const FULL_HEIGHT = 500;
+const imgDataPrefix = 'data:image/png;base64,';
 
 exports.init = function(router) {
     router.post('/imgur/', uploadImageToImgur);
 };
 
 function* uploadImageToImgur() {
-    var imgurRes = yield http.post('https://api.imgur.com/3/image', this.request.body, {
+    const background = new Jimp(FULL_WIDTH, FULL_HEIGHT, 0xbfd1e5ff);
+
+    let image = yield Jimp.read(new Buffer(this.request.body.image, 'base64'));
+    image.autocrop();
+
+    let width = image.bitmap.width;
+    let height = image.bitmap.height;
+    
+    if(width > FULL_WIDTH || height > FULL_HEIGHT) {
+        image.scaleToFit(FULL_WIDTH, FULL_HEIGHT);
+    }
+    
+    width = image.bitmap.width;
+    height = image.bitmap.height;
+
+    background.composite(image, (FULL_WIDTH - width) / 2, (FULL_HEIGHT - height) / 2);
+
+    let base64Img = yield new Promise(function(resolve, reject) {
+        background.getBase64(Jimp.MIME_PNG, function(err, res) {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(res);
+        });
+    });
+
+    const imgurRes = yield http.post('https://api.imgur.com/3/image', base64Img.substring(imgDataPrefix.length), {
         headers: {
             'Authorization': 'Client-ID ' + config.get('imgurClientId')
         }
-    });
+    }).then(res => res.data);
 
-    this.status = imgurRes.data.status;
-    this.body = imgurRes.data.data;
+    this.status = imgurRes.status;
+    this.body = imgurRes.data;
 }
